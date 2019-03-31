69c85a98010f1386e75dc6f60cefd659d22ec37c
Rafael <rafaelsilvarnj@gmail.com>
2019-03-31 18:04:58 -0300

    Added files

---
+++ XamarinFSharp.Droid/MainActivity.fs
@@ -0,0 +1,29 @@
+﻿namespace XamarinFSharp.Droid
+
+open System
+
+open Android.App
+open Android.Content
+open Android.Content.PM
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Android.OS
+open Xamarin.Forms.Platform.Android
+
+type Resources = XamarinFSharp.Droid.Resource
+
+[<Activity (Label = "XamarinFSharp.Droid", Icon = "@mipmap/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity () =
+    inherit FormsAppCompatActivity()
+
+    override this.OnCreate (bundle) =
+        FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+        FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
+
+        base.OnCreate (bundle)
+
+        Xamarin.Forms.Forms.Init (this, bundle)
+
+        this.LoadApplication (new XamarinFSharp.App())
+
